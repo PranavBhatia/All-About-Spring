656b1171139b8bd277646ca9a85642310b89d697
Pranav Bhatia <bhatiapranav1996@gmail.com>
2022-01-19 19:22:15 -0500

    Using Spring Framework to Manage Dependencies

--- spring-in-10-steps/src/main/java/com/learn/spring/basics/springin5steps/SpringIn10StepsApplication.java
+++ spring-in-10-steps/src/main/java/com/learn/spring/basics/springin5steps/SpringIn10StepsApplication.java
@@ -2,17 +2,19 @@ package com.learn.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringIn10StepsApplication {
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearchImpl = new BinarySearchImpl(new QuickSortAlgorithm());
-//		BinarySearchImpl binarySearchImpl = new BinarySearchImpl(new BubbleSortAlgorithm());
+		// Application Context
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringIn10StepsApplication.class,
+				args);
+		BinarySearchImpl binarySearchImpl = applicationContext.getBean(BinarySearchImpl.class);
+
 		int result = binarySearchImpl.binarySearch(new int[] { 12, 4, 6 }, 3);
 		System.out.println(result);
-
-		SpringApplication.run(SpringIn10StepsApplication.class, args);
 	}
 
 }
