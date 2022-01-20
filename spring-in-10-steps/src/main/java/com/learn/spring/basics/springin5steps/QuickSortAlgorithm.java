1240d9cc95d62e126223f0eae0aba0bb39553a60
Pranav Bhatia <bhatiapranav1996@gmail.com>
2022-01-19 19:42:15 -0500

    Dynamic auto wiring - @Primary

--- spring-in-10-steps/src/main/java/com/learn/spring/basics/springin5steps/QuickSortAlgorithm.java
+++ spring-in-10-steps/src/main/java/com/learn/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.learn.spring.basics.springin5steps;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
 	public int[] sort(int[] numbers) {
 		// Logic for Bubble Sort
