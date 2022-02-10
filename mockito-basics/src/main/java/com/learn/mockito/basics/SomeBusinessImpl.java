31f833519b51318e29df6aeb56b54e07ebdd82e3
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2022-02-09 21:25:16 -0500

    Using a Stubs - Disadvantages

--- mockito-basics/src/main/java/com/learn/mockito/basics/SomeBusinessImpl.java
+++ mockito-basics/src/main/java/com/learn/mockito/basics/SomeBusinessImpl.java
@@ -3,6 +3,11 @@ package com.learn.mockito.basics;
 public class SomeBusinessImpl {
 	private DataService dataService;
 
+	public SomeBusinessImpl(DataService dataService) {
+		super();
+		this.dataService = dataService;
+	}
+
 	int findTheGreatestFromAllData() {
 		int[] data = dataService.retrieveAllData();
 		int greatest = Integer.MIN_VALUE;
---
+++ mockito-basics/src/test/java/com/learn/mockito/basics/SomeBusinessStubTest.java
@@ -0,0 +1,24 @@
+package com.learn.mockito.basics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SomeBusinessStubTest {
+
+	@Test
+	void testFindTheGreatestFromAllData() {
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(new DataServiceStub());
+		int result = businessImpl.findTheGreatestFromAllData();
+		assertEquals(24, result);
+	}
+
+}
+
+class DataServiceStub implements DataService {
+
+	@Override
+	public int[] retrieveAllData() {
+		return new int[] { 24, 6, 15 };
+	}
+}
