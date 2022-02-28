858a49314056225c6883a61e09b8a41a89335719
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2022-02-28 09:46:44 -0500

    Exploration

--- springboot-basics/src/main/java/com/learn/springboot/springbootbasics/SpringbootBasicsApplication.java
+++ springboot-basics/src/main/java/com/learn/springboot/springbootbasics/SpringbootBasicsApplication.java
@@ -2,12 +2,17 @@ package com.learn.springboot.springbootbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootBasicsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBasicsApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootBasicsApplication.class, args);
+
+		for (String beanName : applicationContext.getBeanDefinitionNames()) {
+			System.out.println("Bean: " + beanName);
+		}
 	}
 
 }
